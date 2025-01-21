f45ff0ac4a1d5166c884f50116e05538b997dffc
Lison_Kas <lisonfortin@icloud.com>
2025-01-21 12:29:44 +0100

    Une colonne apparait pour l'échiquier !

---
+++ src/game/Chessboard.cpp
@@ -0,0 +1,46 @@
+#include "Chessboard.hpp"
+#include <imgui.h>
+#include <iostream>
+#include <vector>
+#include "quick_imgui/quick_imgui.hpp"
+
+void Chessboard::InitializeBoardList(){
+    for(int i {0}; i<8; i++){
+        std::vector<Square> line_squares;
+        for(int j {0}; j<8; j++){
+            Square new_square;
+            if(i%2==0){
+                new_square.m_color_light = j%2==0 ? false : true;
+            }
+            else {
+                new_square.m_color_light = j%2==0 ? true : false;
+            }
+            line_squares.push_back(new_square);
+        }
+        this->m_boardlist.push_back(line_squares);
+    }
+}
+
+void Chessboard::CreateBoard(){
+    this->InitializeBoardList();
+    int id {0};
+    for(int i {0}; i<8; i++){
+        for(int j {0}; i<8; i++){
+            char name;
+            if(this->m_boardlist[i][j].m_color_light){
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+                // if(i==0) name = '0';else name = 'i';
+            }
+            else {
+                // if(i==0) name = '0';else name = 'i';
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
+            }
+            ImGui::PushID(id);
+            if (ImGui::Button(&name, ImVec2{100.f, 100.f}))
+                    std::cout << "Clicked button\n";
+            ImGui::PopID();
+            ImGui::PopStyleColor();
+            id++;
+        }
+    }
+}
--- src/game/Chessboard.hpp
+++ src/game/Chessboard.hpp
@@ -1,5 +1,17 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "Square.hpp"
 
 class Chessboard {
-    
+    private:
+        std::vector<std::vector<Square>> m_boardlist;
+
+    public:
+        //std::vector<Piece> m_WhitePieces;
+        //std::vector<Piece> m_BlackPieces;
+
+    public:
+        void InitializeBoardList();
+        void CreateBoard();
 };
