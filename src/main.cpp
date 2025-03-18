94417c8cb5d485ec4829ec3a3be2723d5074fb3f
Lison_Kas <lisonfortin@icloud.com>
2025-03-18 11:00:22 +0100

    rajout inutile pour le moment mais on va voir ...

--- src/main.cpp
+++ src/main.cpp
@@ -47,4 +47,17 @@ int main()
             // ImGui::End();
         }
     );
+
+    // quick_imgui::loop(
+    //     "Quick ImGui",
+    //     {
+    //         .init                     = []() { app.InitializeGame();},
+    //         .loop                     = []() { app.StartGame(); },
+    //         .key_callback             = [](int key, int scancode, int action, int mods) { std::cout << "Key: " << key << " Scancode: " << scancode << " Action: " << action << " Mods: " << mods << '\n'; },
+    //         .mouse_button_callback    = [](int button, int action, int mods) { std::cout << "Button: " << button << " Action: " << action << " Mods: " << mods << '\n'; },
+    //         .cursor_position_callback = [](double xpos, double ypos) { std::cout << "Position: " << xpos << ' ' << ypos << '\n'; },
+    //         .scroll_callback          = [](double xoffset, double yoffset) { std::cout << "Scroll: " << xoffset << ' ' << yoffset << '\n'; },
+    //         .window_size_callback     = [](int width, int height) { std::cout << "Resized: " << width << ' ' << height << '\n'; },
+    //     }
+    // );
 }
