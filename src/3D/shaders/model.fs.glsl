3b49d1a2c00e16823c2b064cbcc1e043fb7e0e9b
Lison_Kas <lisonfortin@icloud.com>
2025-04-09 18:57:53 +0200

    maj shaders pour les valeurs Ka, Kd, Ks, Ns en direct

--- src/3D/shaders/model.fs.glsl
+++ src/3D/shaders/model.fs.glsl
@@ -3,13 +3,14 @@
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoord;
+in vec3 Kd;
+in vec3 Ka;
+in vec3 Ks;
+in float Ns;
+flat in int useTexture;
 
-// Uniformes de textures et/ou couleurs
+// Uniforme de texture
 uniform sampler2D texture1;
-uniform vec3 Kd;
-uniform vec3 Ka;
-uniform vec3 Ks;
-uniform float Ns;
 
 // Uniformes pour la lumière
 uniform vec3 lightPos;
@@ -17,7 +18,6 @@ uniform vec3 lightColor;
 
 // Autres Uniformes
 uniform vec3 viewPos;
-uniform bool useTexture;
 
 out vec4 FragColor;
 
@@ -37,10 +37,11 @@ void main()
     vec3 specular = Ks * spec * lightColor;
     
     vec3 texColor;
-    if (useTexture) {
-        texColor = texture(texture1, TexCoord).rgb;
+    if (useTexture != 0) {
+        vec2 uv = TexCoord;
+        texColor = texture(texture1, uv).rgb;
     } else {
-        texColor = vec3(1.0); 
+        texColor = vec3(1.0, 1.0, 1.0); 
     }
     
     vec3 result = (ambient + diffuse) * texColor + specular;
@@ -48,5 +49,4 @@ void main()
     result = pow(result, vec3(1.0/2.2));
     
     FragColor = vec4(result, 1.0);
-    // FragColor = vec4((Normal + 1.0) / 2.0, 1.0);
 }
--- src/3D/shaders/model.vs.glsl
+++ src/3D/shaders/model.vs.glsl
@@ -3,6 +3,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in vec3 aKd;
+layout (location = 4) in vec3 aKs;
+layout (location = 5) in vec3 aKa;
+layout (location = 6) in float aNs;
+layout (location = 7) in int aUseTexture;
 
 // Uniformes pour les matrices
 uniform mat4 model;
@@ -12,12 +17,22 @@ uniform mat4 projection;
 out vec2 TexCoord;
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 Kd;
+out vec3 Ks;
+out vec3 Ka;
+out float Ns;
+flat out int useTexture;
 
 void main()
 {
     FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = -mat3(transpose(inverse(model))) * aNormal;
+    Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoord = aTexCoord;
+    Kd = aKd;
+    Ks = aKs;
+    Ka = aKa;
+    Ns = aNs;
+    useTexture = aUseTexture;
 
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
