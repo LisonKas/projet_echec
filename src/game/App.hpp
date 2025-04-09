01900707e4825e005c6d39fae50c2cf61c4b8759
fochyl <theo.bonnot@hotmail.fr>
2025-04-09 19:01:13 +0200

    simplification startgame

--- src/game/App.hpp
+++ src/game/App.hpp
@@ -1,15 +1,25 @@
 #pragma once
-#include "Chessboard.hpp"
 #include "../3D/Renderer3D.hpp"
+#include "Chessboard.hpp"
+
 
 class App {
-    private :
-        Renderer3D m_renderer;
-        Chessboard m_chessboard;
+private:
+    Renderer3D m_renderer;
+    Chessboard m_chessboard;
 
-    public:
-        void InitializeGame();
-        void StartGame();
-        void Update();
-        void CloseGame();
+    // Nouvelles fonctions privées
+    void handleNameInput();
+    void whoPlaysFirst();
+    void handleGameOver();
+    void resetGame();
+    void displayPromotion();
+    void promotionButton(const char* label, PieceType type);
+    void promoteTo(const std::string& pieceChoisie);
+
+public:
+    void InitializeGame();
+    void StartGame();
+    void Update();
+    void CloseGame();
 };
